11dd25fffe0b76cea3f6a10974d9128b20c41c31
Pierre Beyssac <pb@fasterix.frmug.org>
1997-09-07 16:29:20 +0000

    Whois routine moved to library.
    git-svn-id: file:///usr/local/svn/trunk/autoreg@79 b7d07175-c409-0410-93c4-cd3d8982087c

---
+++ lib/whois.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+#
+# $Id$
+#
+
+$WHOIS="/usr/bin/whois";
+
+sub whois_html {
+   local ($server, $request) = ($_[0], $_[1]);
+   print "<HR>\n";
+   $server =~ s/["';&]//g;
+   $request =~ s/["';&]//g;
+
+   if (!open(WHOIS, "$WHOIS -h \"$server\" \"$request\"|")) {
+      print "<STRONG>Can't execute whois: $!</STRONG>\n";
+      return;
+   }
+   print "<TT>whois -h $server $request</TT>\n";
+   print "<HR><PRE>\n";
+   while (<WHOIS>) {
+      s/</&lt;/g;
+      s/>/&gt;/g;
+      print;
+   }
+   print "</PRE><HR>\n";
+   close(WHOIS);
+}
+
+1;
