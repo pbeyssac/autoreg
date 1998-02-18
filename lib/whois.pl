8510636c1dfcc630e64c53510da5413eac585006
Pierre Beyssac <pb@fasterix.frmug.org>
1998-02-18 00:22:13 +0000

    Improved whois support: 	- don't fork /usr/bin/whois, directly connect to the server 	  instead. 	- new call to get contacts email for a domain.
    git-svn-id: file:///usr/local/svn/trunk/autoreg@139 b7d07175-c409-0410-93c4-cd3d8982087c

--- lib/whois.pl
+++ lib/whois.pl
@@ -3,14 +3,33 @@
 # $Id$
 #
 
-$WHOIS="/usr/bin/whois";
+$WHOIS_PORT = 43;
+$AF_INET = 2;
+$SOCK_STREAM = 1;
+
+sub whois_socket {
+   local ($whoishost, $request) = ($_[0], $_[1]);
+   local ($name, $aliases, $type, $len, $thataddr, $sockaddr, $that);
+
+   $sockaddr = 'S n a4 x8';
+   ($name, $aliases, $type, $len, $thataddr) = gethostbyname($whoishost);
+   $that = pack($sockaddr, $AF_INET, $WHOIS_PORT, $thataddr);
+   socket(WHOIS, $AF_INET, $SOCK_STREAM, $proto) || return "";
+   if (!connect(WHOIS, $that)) {
+      close(WHOIS);
+      return "";
+   }
+   select(WHOIS); $| = 1; select(STDOUT);
+   print WHOIS "$request\r\n";
+   return 1;
+}
 
 sub whois_domain {
    local ($server, $request) = ($_[0], $_[1]);
    $server =~ s/["';&]//g;
    $request =~ s/["';&]//g;
 
-   if (!open(WHOIS, "$WHOIS -h \"$server\" \" -r -T domain $request\"|")) {
+   if (!&whois_socket($server, "-r -T domain $request")) {
       return "Can't execute whois: $!";
    }
    local ($output);
@@ -24,11 +43,11 @@ sub whois_html {
    $server =~ s/["';&]//g;
    $request =~ s/["';&]//g;
 
-   if (!open(WHOIS, "$WHOIS -h \"$server\" \"$request\"|")) {
+   if (!&whois_socket($server, $request)) {
       print "<STRONG>Can't execute whois: $!</STRONG>\n";
-      return;
+      return "";
    }
-   print "<TT>whois -h $server $request</TT>\n";
+   print "<TT>\"$request\" at $server</TT>\n";
    print "<PRE>\n";
    while (<WHOIS>) {
       s/</&lt;/g;
@@ -37,6 +56,31 @@ sub whois_html {
    }
    print "</PRE>\n";
    close(WHOIS);
+}
+
+sub whois_email {
+   local ($server, $request) = ($_[0], $_[1]);
+   local (%emails);
+   $server =~ s/["';&]//g;
+   $request =~ s/["';&]//g;
+
+   if (!&whois_socket($server, "-T domain $request")) {
+      return "";
+   }
+   #
+   # In case "-T domain" doesn't work as expected, skip until
+   # we find a domain.
+   #
+   while (<WHOIS>) {
+      last if (/^domain:/i);
+   }
+   while (<WHOIS>) {
+      if (/^e-mail:\s*(.*)$/i) {
+	$emails{$1} = 1;
+      }
+   }
+   close(WHOIS);
+   return sort keys(%emails);
 }
 
 1;
