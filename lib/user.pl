842d5a663cd44622501df3ee30eb69a1545d3d39
Pierre Beyssac <pb@fasterix.frmug.org>
1998-02-20 23:38:22 +0000

    Move user info stuff.
    git-svn-id: file:///usr/local/svn/trunk/autoreg@150 b7d07175-c409-0410-93c4-cd3d8982087c

---
+++ lib/user.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+#
+# $Id$
+#
+# Access users file.
+
+# local configuration.
+require "/usr/local/dns-manager/conf/config";
+
+sub user_get {
+  local ($remuser) = $_[0];
+
+  if (!open(UP, $PASSFILE)) {
+	return "";
+  }
+  while (<UP>) {
+	next if /^#/;
+	chop;
+	local ($user, $pass, $email, $mntby, $passmnt) = split(/:/);
+	if ($user eq $remuser) {
+	  close UP;
+	  return ($pass, $email, $mntby, $passmnt); }
+  }
+  close (UP);
+  return "";
+}
+
+1;
