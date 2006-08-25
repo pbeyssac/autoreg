5a6180dba4c70a106a056cddc065e761f09be912
Pierre Beyssac <pb@fasterix.frmug.org>
2006-08-25 20:33:23 +0000

    Remove all TABs.
    git-svn-id: file:///usr/local/svn/trunk/autoreg@425 b7d07175-c409-0410-93c4-cd3d8982087c

--- postgres/python/whoisdb.py
+++ postgres/python/whoisdb.py
@@ -92,9 +92,9 @@ def from_ripe(o, attrlist):
     if not k in attrlist:
       dlist.append(k)
       if not k in ['so', 'mb']:
-	for v in o[k]:
+        for v in o[k]:
           print "WARN: Ignoring attribute \"%s\" (%s)" \
-		% (ripe_stol.get(k, k), v)
+                % (ripe_stol.get(k, k), v)
   # cleanup ignored attributes
   for k in dlist:
     del o[k]
@@ -112,11 +112,11 @@ def from_ripe(o, attrlist):
     if not k in o:
       if minl > 0:
         print "ERROR: Missing attribute \"%s\"" % ripe_stol[k]
-	ok = False
+        ok = False
       o[k] = [ None ]
     else:
       if not (minl <= len(o[k]) <= maxl):
-	print "WARN: " \
+        print "WARN: " \
               "Attribute \"%s\" found %d times instead of %d to %d time(s)" % \
               (ripe_stol[k], len(o[k]), minl, maxl)
         o[k] = o[k][:maxl]
@@ -155,8 +155,8 @@ def mkinitials(name):
 class _whoisobject(object):
   def __cmp__(self, other):
     """Customized compare function:
-	- ignore 'ch' and 'cr' entries
-	- case-insensitive compare on 'pn'
+        - ignore 'ch' and 'cr' entries
+        - case-insensitive compare on 'pn'
     """
     if not isinstance(other, type(self)):
       return id(self).__cmp__(id(other))
@@ -196,9 +196,9 @@ class Person(_whoisobject):
 
       # Find the highest allocated handle with the same initials
       self._dbc.execute("SELECT CAST(SUBSTRING(handle FROM '[0-9]+') AS INT)"
-			" FROM contacts WHERE handle SIMILAR TO '%s[0-9]+'"
-			" ORDER BY CAST(SUBSTRING(handle FROM '[0-9]+') AS INT)"
-			" DESC LIMIT 1" % (l,))
+                        " FROM contacts WHERE handle SIMILAR TO '%s[0-9]+'"
+                        " ORDER BY CAST(SUBSTRING(handle FROM '[0-9]+') AS INT)"
+                        " DESC LIMIT 1" % (l,))
       assert 0 <= self._dbc.rowcount <= 1
       if self._dbc.rowcount == 0:
         i = 1
@@ -252,7 +252,7 @@ class Person(_whoisobject):
     """Write back to database."""
     o = self.d
     self._dbc.execute('UPDATE contacts SET handle=%s,exthandle=%s,'
-		      'name=%s,email=%s,'
+                      'name=%s,email=%s,'
                       'addr=%s,phone=%s,fax=%s,updated_by=%s,updated_on=NOW() '
                       'WHERE id=%d',
                       (o['nh'][0], o['eh'][0], o['pn'][0], o['em'][0],
@@ -295,14 +295,14 @@ class Person(_whoisobject):
       if i == 'pn':
         l = title
       elif i in ['nh', 'eh'] and embed:
-	continue
+        continue
       else:
         l = ripe_stol[i]
       for j in d[i]:
         if i == 'ch':
           j = str(j[1])
         elif i == 'nh':
-	  j = suffixadd(j)
+          j = suffixadd(j)
         if j != None:
           s += "%-12s %s\n" % (l+':', j)
     return s
@@ -334,8 +334,8 @@ class Domain(_whoisobject):
     # of the RIPE-style domain object.
     c = {}
     if 'ad' in o:
-	c['pn'] = o['ad'][:1]
-	c['ad'] = o['ad'][1:]
+        c['pn'] = o['ad'][:1]
+        c['ad'] = o['ad'][1:]
         del o['ad']
     c['ch'] = o['ch']
     self.d = o
@@ -394,7 +394,7 @@ class Domain(_whoisobject):
       if not si in o:
         continue
       for v in o[si]:
-	if v == None: continue
+        if v == None: continue
         self._dbc.execute('INSERT INTO domain_contact '
                           '(whoisdomain_id,contact_id,contact_type_id) '
                           'VALUES (%d,%d,'
@@ -462,37 +462,37 @@ class Domain(_whoisobject):
       newd[k] = [ ]
       for l in self.d[k]:
         if l == None: continue
-	ll = l.lower()
-	if ll in prefs and len(prefs[ll]):
-	  cid = prefs[ll][0].cid
+        ll = l.lower()
+        if ll in prefs and len(prefs[ll]):
+          cid = prefs[ll][0].cid
           newd[k].append(cid)
-	  # rotate prefs
+          # rotate prefs
           prefs[ll] = prefs[ll][1:] + prefs[ll][:1]
-	  continue
-	# XXX: "... AND email IS NOT NULL" is a hack
-	# to exclude "registrant" contacts while (temporarily)
-	# allowing regular contacts without an email.
+          continue
+        # XXX: "... AND email IS NOT NULL" is a hack
+        # to exclude "registrant" contacts while (temporarily)
+        # allowing regular contacts without an email.
         if l.upper().endswith(handlesuffix):
           self._dbc.execute('SELECT id FROM contacts'
                             ' WHERE (lower(contacts.name)=%s'
-			    ' AND email IS NOT NULL) OR handle=%s',
+                            ' AND email IS NOT NULL) OR handle=%s',
                             (l.lower(), suffixstrip(l.upper())))
-	else:
+        else:
           self._dbc.execute('SELECT id FROM contacts'
                             ' WHERE (lower(contacts.name)=%s'
-			    ' AND email IS NOT NULL) OR exthandle=%s',
+                            ' AND email IS NOT NULL) OR exthandle=%s',
                             (l.lower(), l.upper()))
         # check the returned number of found lines and
         # issue an approriate warning message if it differs from 1.
         if self._dbc.rowcount == 0:
           print "ERROR: Invalid %s contact '%s' for domain %s" \
-		% (contact_map_rev[k], l, self.d['dn'][0])
+                % (contact_map_rev[k], l, self.d['dn'][0])
           inval += 1
         elif self._dbc.rowcount > 1:
           print "ERROR: Ambiguous key '%s' for domain %s %s contact" \
-		" resolves to %d records" % (l, self.d['dn'][0],
-					     contact_map_rev[k],
-					     self._dbc.rowcount)
+                " resolves to %d records" % (l, self.d['dn'][0],
+                                             contact_map_rev[k],
+                                             self._dbc.rowcount)
           ambig += 1
         lid = self._dbc.fetchall()
         for cid, in lid:
@@ -538,11 +538,11 @@ class Lookup:
     return self._makelist()
   def persons_by_name(self, name):
     self._dbc.execute('SELECT id FROM contacts WHERE lower(name)=%s' \
-		      ' AND email IS NOT NULL', (name.lower(),))
+                      ' AND email IS NOT NULL', (name.lower(),))
     return self._makelist()
   def persons_by_email(self, email):
     self._dbc.execute('SELECT id FROM contacts WHERE lower(email)=%s',
-		      (email.lower(),))
+                      (email.lower(),))
     return self._makelist()
   def domain_by_name(self, name):
     name = name.upper()
@@ -584,13 +584,13 @@ class Main:
       o['ch'] = [ forcechanged ]
     elif 'ch' in o:
       for i in range(len(o['ch'])):
-	email, t = parse_changed(o['ch'][i])
-	if (email, t) == (None, None):
-	  return False
-	if t == None:
-	  # "changed:" line without a date
-	  t = self.now
-	o['ch'][i] = email, t
+        email, t = parse_changed(o['ch'][i])
+        if (email, t) == (None, None):
+          return False
+        if t == None:
+          # "changed:" line without a date
+          t = self.now
+        o['ch'][i] = email, t
     if 'dn' in o:
       # domain object
       i = o['dn'][0].upper()
@@ -599,50 +599,50 @@ class Main:
         ld = self._lookup.domain_by_name(i)
         if ld != None:
           ld.delete()
-	  print "Object deleted:"
-	  ld.display()
-	  return True
+          print "Object deleted:"
+          ld.display()
+          return True
       self.ndom += 1
       ld = self._lookup.domain_by_name(i)
       if ld != None:
-	# domain already exists
+        # domain already exists
         ld.fetch()
         newdom = Domain(self._dbc, ld.did)
         r = newdom.from_ripe(o, persons)
-	if r == None:
-	  # something incorrect in provided attributes
-	  return False
+        if r == None:
+          # something incorrect in provided attributes
+          return False
         ambig, inval = r
-	if ambig or inval:
-	  return False
-	# compare with new object
+        if ambig or inval:
+          return False
+        # compare with new object
         if ld != newdom or ld.ct.d['ad'] != newdom.ct.d['ad']:
-	  # they differ, update database
+          # they differ, update database
           print "Object updated from:"
-	  ld.display()
+          ld.display()
           newdom.ct.cid = ld.ct.cid
           newdom.update()
           print "Object updated to:"
-	  newdom.display()
+          newdom.display()
           self.ambig += ambig
           self.inval += inval
-	else:
-	  print "Object already exists:"
-	  ld.display()
+        else:
+          print "Object already exists:"
+          ld.display()
       else:
-	# make domain object
+        # make domain object
         ld = Domain(self._dbc)
         r = ld.from_ripe(o, persons)
-	if r == None:
-	  # something incorrect in provided attributes
-	  return False
+        if r == None:
+          # something incorrect in provided attributes
+          return False
         ambig, inval = r
-	if ambig or inval:
-	  return False
-	# store to database
+        if ambig or inval:
+          return False
+        # store to database
         ld.insert()
         print "Object created:"
-	ld.display()
+        ld.display()
         self.ambig += ambig
         self.inval += inval
     elif 'pn' in o:
@@ -650,10 +650,10 @@ class Main:
       self.nperson += 1
       ct = Person(self._dbc)
       if not ct.from_ripe(o):
-	return False
+        return False
       name = o['pn'][0].lower()
       if 'eh' in o and o['eh'][0] != None:
-	ehandle = o['eh'][0].lower()
+        ehandle = o['eh'][0].lower()
       else:
         ehandle = None
       if 'nh' in o and o['nh'][0] != None:
@@ -662,44 +662,44 @@ class Main:
         lp = self._lookup.persons_by_handle(handle)
         assert len(lp) <= 1
         if len(lp) == 1:
-	  c = lp[0]
+          c = lp[0]
           # found, compare
           c.fetch()
-	  if dodel:
-	    if ct != c:
+          if dodel:
+            if ct != c:
               print "ERROR: Cannot delete: not the same object"
-	      return False
-	    else:
-	      print "Object deleted:"
-	      ct.display()
+              return False
+            else:
+              print "Object deleted:"
+              ct.display()
               ct.delete()
           else:
             if ct != c:
-	      print "Object updated from:"
-	      c.display()
-	      print "Object updated to:"
-	      ct.cid = c.cid
+              print "Object updated from:"
+              c.display()
+              print "Object updated to:"
+              ct.cid = c.cid
               ct.update()
             else:
-	      ct = c
-	      print "Object already exists:"
+              ct = c
+              print "Object already exists:"
         else:
           # not found
           if dodel:
             print "ERROR: Cannot delete: not found"
-	    return False
+            return False
           else:
             ct.insert()
-	    print "Object created:"
-	if not dodel:
-	  ct.display()
-	  # keep for contact assignment
-	  persons.setdefault(handle, []).append(ct)
-	  persons.setdefault(name, []).append(ct)
-	  persons.setdefault(ehandle, []).append(ct)
+            print "Object created:"
+        if not dodel:
+          ct.display()
+          # keep for contact assignment
+          persons.setdefault(handle, []).append(ct)
+          persons.setdefault(name, []).append(ct)
+          persons.setdefault(ehandle, []).append(ct)
       elif dodel:
-	print "ERROR: Cannot delete: no handle provided"
-	return False
+        print "ERROR: Cannot delete: no handle provided"
+        return False
       else:
         # no handle, try to find by name
         lp = self._lookup.persons_by_name(name)
@@ -710,19 +710,19 @@ class Main:
             o['nh'] = c.d['nh']
             if ct == c:
               # found, stop
-	      ct = c
-	      print "Object already exists:"
+              ct = c
+              print "Object already exists:"
               break
             # clear copied handle
             o['nh'] = [ None ];
         else:
             # not found, insert
             ct.insert()
-	    print "Object created:"
-	ct.display()
-	# keep for contact assignment
-	persons.setdefault(name, []).append(ct)
-	persons.setdefault(ehandle, []).append(ct)
+            print "Object created:"
+        ct.display()
+        # keep for contact assignment
+        persons.setdefault(name, []).append(ct)
+        persons.setdefault(ehandle, []).append(ct)
     elif 'mt' in o:
       # maintainer object, ignore
       pass
@@ -738,7 +738,7 @@ class Main:
   def _order(self, o, dodel, persons, nohandle, domains, forcechanged):
     """Handle reordering."""
     if not dodel and 'pn' in o \
-	and (not 'nh' in o or not o['nh'][0].endswith(handlesuffix)):
+        and (not 'nh' in o or not o['nh'][0].endswith(handlesuffix)):
       # updating or creation of a person object, no handle yet.
       # keep for later allocation to avoid clashes
       nohandle.append(o)
@@ -751,7 +751,7 @@ class Main:
     return True
 
   def parsefile(self, file, encoding='ISO-8859-1', commit=True,
-		forcechangedemail=None):
+                forcechangedemail=None):
     """Parse file and reorder objects before calling process()."""
     o = {}
     persons = {}
@@ -788,8 +788,8 @@ class Main:
       if self.white_re.search(l) and len(o):
         # white line or empty line and o is not empty:
         # end of object, process then cleanup for next object.
-	if not self._order(o, dodel, persons, nohandle, domains, forcechanged):
-	  err += 1
+        if not self._order(o, dodel, persons, nohandle, domains, forcechanged):
+          err += 1
         o = {}
         dodel = False
         continue
@@ -802,42 +802,42 @@ class Main:
         a, v = m.groups()
       else:
         m = self.longattr_re.search(l)
-	if not m:
-	  print "ERROR: Unrecognized line:", l
-	  err += 1
-	  continue
+        if not m:
+          print "ERROR: Unrecognized line:", l
+          err += 1
+          continue
         a, v = m.groups()
         if not a in ripe_ltos:
           print "ERROR: Unrecognized attribute \"%s\"" % a
-	  err += 1
-	  continue
+          err += 1
+          continue
         a = ripe_ltos[a]
       if a == 'delete':
-	# mark for deletion
+        # mark for deletion
         dodel = True
       else:
-	# new or multi-valued attribute
+        # new or multi-valued attribute
         o.setdefault(a, []).append(v)
     # end of file
     if len(o):
       # end of file: process last object
       if not self._order(o, dodel, persons, nohandle, domains, forcechanged):
-	err += 1
+        err += 1
 
     for p in nohandle:
       if not self.process(p, False, persons, forcechanged):
-	err += 1
+        err += 1
 
     # now that contacts are ready to be used, insert domain_contact records
     # from the domain list we gathered.
     for i in sorted(domains.keys()):
       if not self.process(domains[i], False, persons, forcechanged):
-	err += 1
+        err += 1
 
     if commit and err == 0:
-	self._dbh.commit()
+        self._dbh.commit()
     else:
-	self._dbh.rollback()
+        self._dbh.rollback()
 
     print "Domains:", self.ndom
     print "Persons:", self.nperson
