54b0f56483e3c35e1981112f985d7061f65f8848
Pierre Beyssac <pb@fasterix.frmug.org>
2007-08-27 21:41:46 +0000

    Use environment variable AUTOREG_DBSTRING, if present, to override conf.dbstring.
    git-svn-id: file:///usr/local/svn/trunk/autoreg@467 b7d07175-c409-0410-93c4-cd3d8982087c

--- postgres/python/autoreg/conf.py
+++ postgres/python/autoreg/conf.py
@@ -1,6 +1,8 @@
 #
 # $Id$
 
+import os
+
 msgdir='/usr/local/dns-manager/conf'
 zones_auth='/usr/local/dns-manager/conf/zones-auth'
-dbstring='dbname=eudevel'
+dbstring=os.getenv('AUTOREG_DBSTRING') or 'dbname=eu.org'
