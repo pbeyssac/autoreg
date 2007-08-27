c79ea79fda46594ae58ac320e3e8531622e30fa1
Pierre Beyssac <pb@fasterix.frmug.org>
2007-08-27 19:16:20 +0000

    Add missing files for a Python package.
    git-svn-id: file:///usr/local/svn/trunk/autoreg@465 b7d07175-c409-0410-93c4-cd3d8982087c

---
+++ postgres/python/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='autoreg',
+      version='0.1',
+      author='Pierre Beyssac', author_email='autoreg-devel@eu.org',
+      url='http://eu.org/',
+      packages=['autoreg', 'autoreg.dns', 'autoreg.whois']
+      )
