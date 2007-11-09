9673b448b5300b1d0d896ee81a29819832c4cb59
Pierre Beyssac <pb@fasterix.frmug.org>
2007-11-09 23:48:48 +0000

    Tests!
    git-svn-id: file:///usr/local/svn/trunk/autoreg@498 b7d07175-c409-0410-93c4-cd3d8982087c

---
+++ postgres/python/test-dns-db.py
@@ -0,0 +1,190 @@
+#!/usr/local/bin/python
+# $Id$
+
+import cStringIO
+import re
+import os
+import sys
+
+import psycopg
+
+import autoreg.dns.db
+
+import unittest
+
+class TestDnsDb(unittest.TestCase):
+  re_do = re.compile('^; domain (\S+)$')
+  re_zo = re.compile('^; zone (\S+)$')
+  re_cr = re.compile('^; created: by (\S+), (\S+ \S+)$')
+  re_up = re.compile('^; updated: by (\S+), (\S+ \S+)$')
+  ns12 = """				NS	NS1.EU.ORG.
+				NS	NS2.EU.ORG.
+"""
+  null = ""
+  ns345 = """				NS	NS3.EU.ORG.
+				NS	NS4.EU.ORG.
+				NS	NS5.EU.ORG.
+"""
+  glue12 = """				NS	NS1.TESTGL.EU.ORG.
+				NS	NS2.TESTGL.EU.ORG.
+NS1.TESTGL			A	1.2.3.4
+NS2.TESTGL			AAAA	::FFFF:10.1.2.3
+"""
+  def _parseout(self, d, z):
+    oldstdout = sys.stdout
+    fout = cStringIO.StringIO()
+    sys.stdout = fout
+    self.dd.show(d, z)
+    flags = []
+    dom, zone, crby, upby = None, None, None, None
+    rest = ''
+    for l in fout.getvalue().split('\n'):
+      if l == '; registry_hold' or l == '; registry_lock' or l == '; internal':
+        flags.append(l[2:])
+        continue
+      if l == '; (NO RECORD)':
+        continue
+      m = self.re_do.match(l)
+      if m:
+        dom = m.groups()[0]
+        continue
+      m = self.re_zo.match(l)
+      if m:
+        zone = m.groups()[0]
+        continue
+      m = self.re_cr.match(l)
+      if m:
+        crby = m.groups()[0]
+        continue
+      m = self.re_up.match(l)
+      if m:
+        upby = m.groups()[0]
+        continue
+      rest += l + '\n'
+    sys.stdout = oldstdout
+    return dom, zone, crby, upby, flags, rest
+  def _dropdb(self):
+    dbh = psycopg.connect('dbname=template1')
+    dbh.autocommit(True)
+    dbc = dbh.cursor()
+    try:
+      dbc.execute("DROP DATABASE eutest")
+    #except psycopg.ProgrammingError('database "eutest" does not exist'):
+    except psycopg.ProgrammingError:
+      pass
+  def setUp(self):
+    self._dropdb()
+    dbh = psycopg.connect('dbname=template1')
+    dbh.autocommit(True)
+    dbc = dbh.cursor()
+    dbc.execute("CREATE DATABASE eutest WITH ENCODING='UTF-8'")
+    del dbc
+    del dbh
+    os.system("psql eutest < ../eu.org.schema >/dev/null 2>&1")
+    self.dbh = psycopg.connect('dbname=eutest')
+    dbc = self.dbh.cursor()
+    dbc.execute("INSERT INTO zones (name, soaprimary, soaemail, soaserial)"
+                " VALUES ('EU.ORG', 'NS.EU.ORG', 'hostmaster.eu.org',"
+                "2007110600)")
+    dbc.execute("INSERT INTO allowed_rr (zone_id, rrtype_id)"
+                " VALUES ((SELECT id FROM zones WHERE name='EU.ORG'),"
+                "(SELECT id FROM rrtypes WHERE label='NS'))")
+    self.dd = autoreg.dns.db.db(self.dbh)
+    self.dd.login('DNSADMIN')
+  def tearDown(self):
+    del self.dd
+    del self.dbh
+    self._dropdb()
+  def _test_base(self, dom, zone, internal, val1, val2):
+    zone = 'EU.ORG'
+    fqdn = dom + '.' + zone
+    expect_flags = []
+    if internal:
+      expect_flags.append('internal')
+    f1 = val1
+    f2 = val2
+    of1 = val1 + '\n'
+    if val1 != '':
+      of1 = dom + of1
+    of2 = val2 + '\n'
+    if val2 != '':
+      of2 = dom + of2
+    self.dd.new(fqdn, zone, 'NS', file=cStringIO.StringIO(f1),
+                internal=internal)
+    self.assertRaises(autoreg.dns.db.DomainError,
+                      self.dd.new, fqdn, zone, 'NS',
+                      file=cStringIO.StringIO(f1))
+    self.assertEqual(self._parseout(fqdn, zone),
+                     (fqdn, zone,
+                      '*unknown*', '*unknown*', expect_flags, of1))
+    self.dd.set_registry_lock(fqdn, zone, True)
+    self.assertEqual(self._parseout(fqdn, zone),
+                     (fqdn, zone,
+                      '*unknown*', '*unknown*',
+                      ['registry_lock'] + expect_flags,
+                      of1))
+    self.dd.set_registry_hold(fqdn, zone, True)
+    self.assertEqual(self._parseout(fqdn, zone),
+                     (fqdn, zone,
+                      '*unknown*', '*unknown*',
+                      ['registry_lock', 'registry_hold'] + expect_flags,
+                      of1))
+    self.assertRaises(autoreg.dns.db.AccessError,
+                      self.dd.delete, fqdn, zone)
+    self.assertRaises(autoreg.dns.db.AccessError,
+                      self.dd.modify, fqdn, zone, 'NS',
+                      file=cStringIO.StringIO(f2))
+    self.dd.set_registry_lock(fqdn, zone, False)
+    self.assertEqual(self._parseout(fqdn, zone),
+                     (fqdn, zone,
+                      '*unknown*', '*unknown*',
+                      ['registry_hold'] + expect_flags, of1))
+    if internal:
+      self.assertRaises(autoreg.dns.db.AccessError,
+                        self.dd.modify, fqdn, zone, 'NS',
+                        file=cStringIO.StringIO(f2))
+      self.assertEqual(self._parseout(fqdn, zone),
+                       (fqdn, zone,
+                        '*unknown*', '*unknown*',
+                        ['registry_hold'] + expect_flags, of1))
+      self.assertRaises(autoreg.dns.db.AccessError,
+                        self.dd.delete, fqdn, zone)
+      self.assertEqual(self._parseout(fqdn, zone),
+                       (fqdn, zone,
+                        '*unknown*', '*unknown*',
+                        ['registry_hold'] + expect_flags, of1))
+      self.dd.modify(fqdn, zone, 'NS',
+                     file=cStringIO.StringIO(f2),
+                     override_internal=True)
+      self.assertEqual(self._parseout(fqdn, zone),
+                       (fqdn, zone,
+                        '*unknown*', '*unknown*',
+                        ['registry_hold'] + expect_flags, of2))
+      self.dd.delete(fqdn, zone, override_internal=True)
+    else:
+      self.dd.modify(fqdn, zone, 'NS', file=cStringIO.StringIO(f2))
+      self.assertEqual(self._parseout(fqdn, zone),
+                       (fqdn, zone,
+                        '*unknown*', '*unknown*',
+                        ['registry_hold'] + expect_flags, of2))
+      self.dd.delete(fqdn, zone)
+    self.assertRaises(autoreg.dns.db.DomainError,
+                      self.dd.delete, fqdn, zone)
+  def test1(self):
+    self._test_base('TEST1', 'EU.ORG', False, self.ns12, self.ns345)
+  def test1i(self):
+    self._test_base('TEST1I', 'EU.ORG', True, self.ns12, self.ns345)
+  def test2(self):
+    self._test_base('TESTGL', 'EU.ORG', False, self.glue12, self.ns345)
+  def test2c(self):
+    self._test_base('TESTGL', 'EU.ORG', False, self.ns345, self.glue12)
+  def test2u(self):
+    self._test_base('TESTGL', 'EU.ORG', False, self.glue12, self.glue12)
+  def test3a(self):
+    self._test_base('TESTNL', 'EU.ORG', False, self.null, self.ns12)
+  def test3b(self):
+    self._test_base('TESTNL', 'EU.ORG', False, self.ns12, self.null)
+            
+if __name__ == '__main__':
+  unittest.main()
+
---
+++ postgres/python/test-dns-parser.py
@@ -0,0 +1,95 @@
+#!/usr/local/bin/python
+# $Id$
+
+import autoreg.dns.parser
+
+import unittest
+
+class TestDnsParser(unittest.TestCase):
+  def setUp(self):
+    self.p = autoreg.dns.parser.DnsParser()
+  def _test_type_val(self, rrtype, rrvallist,
+                     force_upper=False):
+    for rrval in rrvallist:
+      retval = rrval
+      if force_upper:
+        retval = retval.upper()
+      for label in ['', 'Z', 'z', 'z_1', 'z-1', '1234', 'ab.cd',
+                    'ab.cd.ef', 'ab.cd.ef.gh', '1234567890'*6, '1234567890'*7,
+                    '*', '*.aa']:
+        for rrtypem in rrtype.lower(), rrtype.upper():
+          for inclass in 'IN ', '':
+            self.assertEqual(self.p.parseline("%s %s %s %s"
+                                              % (label, inclass,
+                                                 rrtypem, rrval)),
+                             (label.upper(), None, rrtype.upper(), retval))
+            self.assertEqual(self.p.parseline("%s %s %s %s "
+                                              % (label, inclass,
+                                                 rrtypem, rrval)),
+                             (label.upper(), None, rrtype.upper(), retval))
+            for ttl in ['0', '600', '3600', '86400', '172800']:
+              self.assertEqual(self.p.parseline("%s %s %s %s %s"
+                                                % (label, ttl, inclass,
+                                                   rrtypem, rrval)),
+                               (label.upper(), ttl, rrtype.upper(), retval))
+              self.assertEqual(self.p.parseline("%s %s %s %s %s "
+                                                % (label, ttl, inclass,
+                                                   rrtypem, rrval)),
+                               (label.upper(), ttl, rrtype.upper(), retval))
+  def test_nok(self):
+    for l in [' A ', 'z', 'z A', ' 3600 A', ' TXT ', ' 1D A 1.2.3.4']:
+      self.assertRaises(autoreg.dns.parser.ParseError, self.p.parseline, l)
+  def test_ok(self):
+    for l in ['', ' ', '\t']:
+      self.assertEqual(self.p.parseline(l), None)
+  def testa_ok(self):
+    self._test_type_val('A', ['10.1.2.3', '0.0.0.0', '255.255.255.255'])
+  def testa_nok(self):
+    for addr in ['127', '1278.0.0.1', '127.0.0.1/8', '1.2.3.455',
+                 '01.02.03.04', 'aaaa', '0.0.0.0-1']:
+      self.assertRaises(autoreg.dns.parser.ParseError, self.p.parseline,
+                        ' A %s' % addr)
+  def testaaaa_ok(self):
+    self._test_type_val('AAAA', ['::', '::1', '2001::', '1:2:3:4:5:6:7:8',
+                                 '12:34:56:78:9a:bc:de:f0',
+                                 '123:456:789:abc:def:012:345:678',
+                                 '1234:5678:9abc:def0:1234:5678:9abc:def0',
+                                 '1234::1', '1234:5678::2',
+                                 '1234:5678:9abc::3', '1234:5678:9abc:def0::4',
+                                 '1234:5678:9abc:def0:1234::5',
+                                 '1234:5678:9abc:def0:1234:5678::6',
+                                 'ff80::1',
+                                 '::10.1.2.3',
+                                 '::ffff:10.1.2.3'],
+                        force_upper=True)
+  def testaaaa_nok(self):
+    for addr in [':::', '1.2.3.4', '::1::']:
+      self.assertRaises(autoreg.dns.parser.ParseError, self.p.parseline,
+                        ' AAAA %s' % addr)
+  def testns_ok(self):
+    self._test_type_val('NS', ['ns.eu.org.', 'signal', 'a.root-servers.net.'],
+                        force_upper=True)
+  def testptr_ok(self):
+    self._test_type_val('PTR', ['ns.eu.org.', 'signal', 'a.root-servers.net.'])
+  def testcname_ok(self):
+    self._test_type_val('CNAME',
+                        ['ns.eu.org.', 'signal', 'a.root-servers.net.'],
+                        force_upper=True)
+  def testmx_ok(self):
+    self._test_type_val('MX', ['10 eu.org.', '0 blablabla.org.', '255 signal',
+                               '20 ns.eu.org.'],
+                     
+                        force_upper=True)
+  def testmx_nok(self):
+    for mx in ['0', '-100 eu.org.', '1000 eu.org', '256 eu.org']:
+      self.assertRaises(autoreg.dns.parser.ParseError, self.p.parseline,
+                        ' MX %s' % mx)
+  def testtxt_ok(self):
+    self._test_type_val('TXT', ['"texte 1"', '"texte2"'])
+  def testsrv_ok(self):
+    self._test_type_val('SRV', ['0 ns.eu.org.', '100 eu.org.'],
+                        force_upper=True)
+
+if __name__ == '__main__':
+  unittest.main()
+
