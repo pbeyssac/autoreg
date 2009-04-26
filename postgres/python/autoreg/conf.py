1c695478ff4fc8fd78b7a1ac49804e67107e14cc
Pierre Beyssac <pb@fasterix.frmug.org>
2009-04-26 12:20:32 +0000

    Forgotten commit from r533.
    git-svn-id: file:///usr/local/svn/trunk/autoreg@535 b7d07175-c409-0410-93c4-cd3d8982087c

--- postgres/python/autoreg/conf.py
+++ postgres/python/autoreg/conf.py
@@ -7,4 +7,12 @@ autoregdir='/usr/local/autoreg'
 confdir=os.path.join(autoregdir, 'conf')
 msgdir=confdir
 zones_auth=os.path.join(confdir,'zones-auth')
+
 dbstring=os.getenv('AUTOREG_DBSTRING') or 'dbname=eu.org'
+
+# export database name in a form suitable for Django
+if dbstring.startswith('dbname='):
+  DATABASE_NAME=dbstring[7:]
+else:
+  # let's try that in case it works by pure chance...
+  DATABASE_NAME=dbstring
