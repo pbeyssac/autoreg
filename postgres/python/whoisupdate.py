ca479b693b58093d87042e94b75b8b263fd75dfb
Pierre Beyssac <pb@fasterix.frmug.org>
2009-05-12 21:27:09 +0000

    Implement option -e to select a different encoding than the default ISO-8859-1.
    git-svn-id: file:///usr/local/svn/trunk/autoreg@613 b7d07175-c409-0410-93c4-cd3d8982087c

--- postgres/python/whoisupdate.py
+++ postgres/python/whoisupdate.py
@@ -1,26 +1,42 @@
 #!/usr/local/bin/python
 # $Id$
 
-import psycopg2
+import getopt
 import sys
+
+import psycopg2
 
 import autoreg.conf
 import autoreg.whois.db as whoisdb
+
+encoding = 'ISO-8859-1'
+
+def usage(argv):
+  print "Usage: %s [-e encoding] [-n]" % argv[0]
 
 def main():
   dbh = psycopg2.connect(autoreg.conf.dbstring)
   w = whoisdb.Main(dbh)
 
+  try:
+    optlist, args = getopt.getopt(sys.argv[1:], 'ne:')
+  except getopt.GetoptError, err:
+    print str(err)
+    usage(sys.argv)
+    sys.exit(2)
+
+  commit = True
+  for opt, val in optlist:
+    if opt == '-n':
+      # Dry run
+      commit = False
+    elif opt == '-e':
+      encoding = val
+
   # to avoid deadlock, read everything on input first
   lines = sys.stdin.readlines()
 
-  if len(sys.argv) >= 2 and sys.argv[1] == '-n':
-    # Dry run
-    commit = False
-  else:
-    commit = True
-
-  if w.parsefile(lines, 'ISO-8859-1', commit):
+  if w.parsefile(lines, encoding, commit):
     print "STATUS OK"
   else:
     print "STATUS ERR"
