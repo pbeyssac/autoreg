6587823afc66c47d6b479db3b324a82491195355
Pierre Beyssac <pb@fasterix.frmug.org>
2016-09-22 19:55:01 +0200

    Handle non-ASCII chars in passwords.

--- autoreg/arf/whois/contactauth.py
+++ autoreg/arf/whois/contactauth.py
@@ -16,7 +16,8 @@ class AuthBackend:
     if login_valid:
       ct = ctlist[0]
       cryptpass = ct.passwd
-      pwd_valid = cryptpass and crypt.crypt(password, cryptpass) == cryptpass
+      pwd_valid = cryptpass and crypt.crypt(password.encode('UTF-8'),
+                                            cryptpass) == cryptpass
     else:
       pwd_valid = False
     if login_valid and pwd_valid:
--- autoreg/arf/whois/views.py
+++ autoreg/arf/whois/views.py
@@ -68,7 +68,7 @@ def _pwcrypt(passwd):
   salt_chars = '0123456789abcdefghijklmnopqstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/.'
   t = ''.join(random.SystemRandom().choice(salt_chars) \
               for i in range(CRYPT_SALT_LEN))
-  return crypt.crypt(passwd, CRYPT_ALGO + t + '$')
+  return crypt.crypt(passwd.encode('UTF-8'), CRYPT_ALGO + t + '$')
 
 def _token_find(contact_id, action):
   """Find existing token(s)"""
@@ -525,7 +525,7 @@ def chpass(request):
       raise SuspiciousOperation
 
     ct = ctlist[0]
-    if ct.passwd != crypt.crypt(pass0, ct.passwd):
+    if ct.passwd != crypt.crypt(pass0.encode('UTF-8'), ct.passwd):
       vars['msg'] = _("Current password is not correct")
       vars = RequestContext(request, vars)
       return render(request, 'whois/chpass.html', vars)
