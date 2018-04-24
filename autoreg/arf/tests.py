434cce7853e6d23e5c562b8cc0166b9a6d96fc97
Pierre Beyssac <pb@fasterix.frmug.org>
2018-04-24 20:17:44 +0200

    Disable HTML escaping in mails, where needed.

--- autoreg/arf/tests.py
+++ autoreg/arf/tests.py
@@ -18,7 +18,7 @@ class RenderMailTestCase(TestCase):
                                ['foobar2@local'], request=None, language=None)
     self.assertEqual(msg, """From: foobar@local
 To: foobar2@local
-Subject: =?utf-8?Q?le=20m=C3=B8=C3=B8se=20=C3=A0=20l&#39;=C3=A9cole?=
+Subject: =?utf-8?Q?le=20m=C3=B8=C3=B8se=20=C3=A0=20l'=C3=A9cole?=
 X-Origin: arf
 Mime-Version: 1.0
 Content-Type: text/plain; charset=utf-8
